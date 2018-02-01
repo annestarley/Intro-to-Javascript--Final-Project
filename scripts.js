2bca277d332e7730ac6a61ef506e12c0ab2fc01a
Anne Starley <annestarley@gmail.com>
2018-02-01 14:17:12 -0800

    added screenshots

---
+++ scripts.js
@@ -0,0 +1,656 @@
+var movies = [{
+    "title": "The Lord of the Rings: The Two Towers - Extended Edition  ",
+    "Drama": true,
+    "Action": true,
+    "Musical": false,
+    "Kids": true,
+    "Workout": false,
+    "Comedy": false,
+    "rating": "PG-13",
+    "image": "./images/lotr.jpg",
+    "format": "DVD",
+    "notes": "",
+  },
+
+  {
+    "title": "Raising Arizona  ",
+    "Drama": false,
+    "Action": true,
+    "Musical": false,
+    "Kids": false,
+    "Workout": false,
+    "Comedy": true,
+    "rating": "PG-13",
+    "image": "./images/raising-arizona.jpg",
+    "format": "DVD",
+    "notes": "",
+  },
+
+  {
+    "title": "The Princess Bride  ",
+    "Comedy": true,
+    "Drama": false,
+    "Action": true,
+    "Musical": false,
+    "Kids": true,
+    "Workout": false,
+    "rating": "PG",
+    "image": "./images/the-princess-bride.jpg",
+    "format": "DVD",
+    "notes": "",
+  },
+
+  {
+    "title": "Pushing Daisies  ",
+    "Comedy": true,
+    "Drama": true,
+    "Action": false,
+    "Musical": false,
+    "Kids": false,
+    "Workout": false,
+    "rating": "PG-13",
+    "image": "./images/pushing-daisies.png",
+    "format": "DVD",
+    "notes": "I should probably return this to Mikayla at some time...",
+  },
+
+  {
+    "title": "Star Wars: Attack of the Clones  ",
+    "Comedy": false,
+    "Drama": true,
+    "Action": true,
+    "Musical": false,
+    "Kids": true,
+    "Workout": false,
+    "rating": "PG-13",
+    "image": "./images/star-wars-attack-of-the-clones.jpg",
+    "format": "DVD",
+    "notes": "",
+  },
+
+  {
+    "title": "Yoga Meltdown  ",
+    "Comedy": false,
+    "Drama": false,
+    "Action": false,
+    "Musical": false,
+    "Kids": false,
+    "Workout": true,
+    "rating": "no rating",
+    "image": "./images/yoga-meltdown.jpg",
+    "format": "DVD",
+    "notes": "",
+  },
+
+  {
+    "title": "Dumb and Dumber - 1 & 2  ",
+    "Drama": false,
+    "Action": false,
+    "Musical": false,
+    "Kids": false,
+    "Workout": false,
+    "Comedy": false,
+    "rating": "PG-13",
+    "image": "./images/dumb-and-dumber.jpg",
+    "format": "DVD",
+    "notes": "",
+  },
+
+  {
+    "title": "Rabbit Proof Fence  ",
+    "Drama": true,
+    "Action": false,
+    "Musical": false,
+    "Kids": true,
+    "Workout": false,
+    "Comedy": false,
+    "rating": "PG",
+    "image": "./images/rabbit-proof-fence.jpg",
+    "format": "DVD",
+    "notes": "Maybe kids? I saw it when I was a kid..",
+  },
+
+  {
+    "title": "The Four Feathers  ",
+    "Drama": true,
+    "Action": true,
+    "Musical": false,
+    "Kids": false,
+    "Workout": false,
+    "Comedy": false,
+    "rating": "PG-13",
+    "image": "./images/the-four-feathers.jpg",
+    "format": "DVD",
+    "notes": "",
+  },
+
+  {
+    "title": "Romancing the Stone  ",
+    "Drama": true,
+    "Action": true,
+    "Musical": false,
+    "Kids": false,
+    "Workout": false,
+    "Comedy": true,
+    "rating": "PG",
+    "image": "./images/romancing-the-stone.jpg",
+    "format": "DVD",
+    "notes": "The 80s kind of PG..",
+  },
+
+  {
+    "title": "King Kong  ",
+    "Drama": true,
+    "Action": true,
+    "Musical": false,
+    "Kids": false,
+    "Workout": false,
+    "Comedy": false,
+    "rating": "PG-13",
+    "image": "./images/king-kong.jpg",
+    "format": "DVD",
+    "notes": "",
+  },
+
+  {
+    "title": "What's Eating Gilbert Grape  ",
+    "Drama": true,
+    "Action": false,
+    "Musical": false,
+    "Kids": false,
+    "Workout": false,
+    "Comedy": false,
+    "rating": "",
+    "image": "./images/what's-eating-gilbert-grape.jpg",
+    "format": "DVD",
+    "notes": "",
+  },
+
+  {
+    "title": "Hail, Caesar!  ",
+    "Drama": false,
+    "Action": true,
+    "Musical": false,
+    "Kids": false,
+    "Workout": false,
+    "Comedy": true,
+    "rating": "PG-13",
+    "image": "./images/hail-caesar.jpg",
+    "format": "DVD",
+    "notes": "",
+  },
+
+  {
+    "title": "Children of Men  ",
+    "Drama": true,
+    "Action": true,
+    "Musical": false,
+    "Kids": false,
+    "Workout": false,
+    "Comedy": false,
+    "rating": "R",
+    "image": "./images/children-of-men.jpg",
+    "format": "DVD",
+    "notes": "",
+  },
+
+  {
+    "title": "Gattaca  ",
+    "Drama": true,
+    "Action": true,
+    "Musical": false,
+    "Kids": false,
+    "Workout": false,
+    "Comedy": false,
+    "rating": "PG-13",
+    "image": "./images/gattaca.jpg",
+    "format": "DVD",
+    "notes": "",
+  },
+
+  {
+    "title": "That Thing You Do  ",
+    "Drama": true,
+    "Action": false,
+    "Musical": false,
+    "Kids": true,
+    "Workout": false,
+    "Comedy": true,
+    "rating": "PG",
+    "image": "./images/that-thing-you-do.jpg",
+    "format": "DVD",
+    "notes": "Almost a musical",
+  },
+
+  {
+    "title": "Star Wars: Revenge of the Sith  ",
+    "Drama": true,
+    "Action": true,
+    "Musical": false,
+    "Kids": true,
+    "Workout": false,
+    "Comedy": false,
+    "rating": "PG-13",
+    "image": "./images/revenge-of-the-sith.jpg",
+    "format": "DVD",
+    "notes": "",
+  },
+
+  {
+    "title": "Troy  ",
+    "Drama": true,
+    "Action": true,
+    "Musical": false,
+    "Kids": false,
+    "Workout": false,
+    "Comedy": false,
+    "rating": "R",
+    "image": "./images/troy.jpg",
+    "format": "DVD",
+    "notes": "",
+  },
+
+  {
+    "title": "Amelie  ",
+    "Drama": true,
+    "Action": false,
+    "Musical": false,
+    "Kids": false,
+    "Workout": false,
+    "Comedy": true,
+    "rating": "R",
+    "image": "./images/amelie.jpg",
+    "format": "DVD",
+    "notes": "",
+  }, {
+    "title": "Miss Pettigrew Lives For A Day  ",
+    "Drama": true,
+    "Action": false,
+    "Musical": false,
+    "Kids": false,
+    "Workout": false,
+    "Comedy": true,
+    "rating": "PG-13",
+    "image": "./images/miss-pettigrew-lives-for-a-day.jpg",
+    "format": "DVD",
+    "notes": "",
+  }, {
+    "title": "Office Space  ",
+    "Drama": false,
+    "Action": false,
+    "Musical": false,
+    "Kids": false,
+    "Workout": false,
+    "Comedy": true,
+    "rating": "R",
+    "image": "./images/office-space.jpg",
+    "format": "DVD",
+    "notes": "",
+  }, {
+    "title": "Mad Max: Fury Road  ",
+    "Drama": true,
+    "Action": true,
+    "Musical": false,
+    "Kids": false,
+    "Workout": false,
+    "Comedy": false,
+    "rating": "R",
+    "image": "./images/mad-max.jpg",
+    "format": "DVD",
+    "notes": "",
+  }, {
+    "title": "Ex Machina  ",
+    "Drama": true,
+    "Action": true,
+    "Musical": false,
+    "Kids": false,
+    "Workout": false,
+    "Comedy": false,
+    "rating": "R",
+    "image": "./images/ex-machina.jpg",
+    "format": "DVD",
+    "notes": "",
+  }, {
+    "title": "Fiddler on the Roof  ",
+    "Drama": true,
+    "Action": false,
+    "Musical": true,
+    "Kids": true,
+    "Workout": false,
+    "Comedy": false,
+    "rating": "G",
+    "image": "./images/fiddler-on-the-roof.jpg",
+    "format": "DVD",
+    "notes": "",
+  }, {
+    "title": "The Intouchables  ",
+    "Drama": true,
+    "Action": false,
+    "Musical": false,
+    "Kids": false,
+    "Workout": false,
+    "Comedy": false,
+    "rating": "R",
+    "image": "./images/the-intouchables.jpg",
+    "format": "DVD",
+    "notes": "",
+  }, {
+    "title": "P90X: Yoga X  ",
+    "Drama": false,
+    "Action": false,
+    "Musical": false,
+    "Kids": false,
+    "Workout": true,
+    "Comedy": false,
+    "rating": "no rating",
+    "image": "./images/p90x-yoga.jpg",
+    "format": "DVD",
+    "notes": "",
+  }, {
+    "title": "P90X: Cardio X  ",
+    "Drama": false,
+    "Action": false,
+    "Musical": false,
+    "Kids": false,
+    "Workout": true,
+    "Comedy": false,
+    "rating": "no rating",
+    "image": "./images/p90x-cardio.jpg",
+    "format": "DVD",
+    "notes": "",
+  }, {
+    "title": "Man of Steel  ",
+    "Drama": true,
+    "Action": true,
+    "Musical": false,
+    "Kids": false,
+    "Workout": false,
+    "Comedy": false,
+    "rating": "PG-13",
+    "image": "./images/man-of-steel.jpg",
+    "format": "Computer File",
+    "notes": "",
+  }, {
+    "title": "The Grand Budapest Hotel  ",
+    "Drama": true,
+    "Action": true,
+    "Musical": false,
+    "Kids": false,
+    "Workout": false,
+    "Comedy": true,
+    "rating": "R",
+    "image": "./images/the-grand-budapest-hotel.jpg",
+    "format": "Computer File",
+    "notes": "",
+  }, {
+    "title": "The Lego Movie  ",
+    "Drama": false,
+    "Action": false,
+    "Musical": false,
+    "Kids": true,
+    "Workout": false,
+    "Comedy": true,
+    "rating": "G",
+    "image": "./images/the-lego-movie.jpg",
+    "format": "Computer File",
+    "notes": "",
+  }
+]
+
+// COMEDIES
+$("#Comedy").click(function(event) {
+  var buttonPushed = event.target;
+  var genre = Comedy //buttonPushed.id;
+  var container = $("#container")
+  container.empty();
+
+  for (var i = 0; i < movies.length; i++) {
+    if (movies[i].Comedy) //[genre])
+    {
+
+      var movieImg = $("<img>");
+      if (movies[i].image) {
+        movieImg.attr('src', movies[i].image);
+      } else {
+        movieImg.attr('src', "http://lorempixel.com/32/32");
+      }
+      container.append(movieImg);
+
+      var title = $('<h3>');
+      title.text(movies[i].title);
+      container.append(title);
+
+      var format = $("<div>");
+      format.text(movies[i].format);
+      format.addClass("format");
+      container.append(format);
+
+      var rating = $("<p>");
+      rating.text(movies[i].rating);
+      container.append(rating);
+
+      var notes = $("<p>");
+      notes.text(movies[i].notes);
+      container.append(notes);
+
+      var line = ("<hr>");
+      container.append(line);
+    }
+  }
+})
+
+
+// Drama
+$("#Drama").click(function(event) {
+  var buttonPushed = event.target;
+  var genre = Drama //buttonPushed.id;
+  var container = $("#container")
+  container.empty();
+
+  for (var i = 0; i < movies.length; i++) {
+    if (movies[i].Drama) //[genre])
+    {
+
+      var movieImg = $("<img>");
+      if (movies[i].image) {
+        movieImg.attr('src', movies[i].image);
+      } else {
+        movieImg.attr('src', "http://lorempixel.com/32/32");
+      }
+      container.append(movieImg);
+
+      var title = $('<h3>');
+      title.text(movies[i].title);
+      container.append(title);
+
+      var format = $("<div>");
+      format.text(movies[i].format);
+      format.addClass("format");
+      container.append(format);
+
+      var rating = $("<p>");
+      rating.text(movies[i].rating);
+      container.append(rating);
+
+      var notes = $("<p>");
+      notes.text(movies[i].notes);
+      container.append(notes);
+
+      var line = ("<hr>");
+      container.append(line);
+    }
+  }
+})
+
+
+//Action
+$("#Action").click(function(event) {
+  var buttonPushed = event.target;
+  var genre = Action //buttonPushed.id;
+  var container = $("#container")
+  container.empty();
+
+  for (var i = 0; i < movies.length; i++) {
+    if (movies[i].Action) //[genre])
+    {
+
+      var movieImg = $("<img>");
+      if (movies[i].image) {
+        movieImg.attr('src', movies[i].image);
+      } else {
+        movieImg.attr('src', "http://lorempixel.com/32/32");
+      }
+      container.append(movieImg);
+
+      var title = $('<h3>');
+      title.text(movies[i].title);
+      container.append(title);
+
+      var format = $("<div>");
+      format.text(movies[i].format);
+      format.addClass("format");
+      container.append(format);
+
+      var rating = $("<p>");
+      rating.text(movies[i].rating);
+      container.append(rating);
+
+      var notes = $("<p>");
+      notes.text(movies[i].notes);
+      container.append(notes);
+
+      var line = ("<hr>");
+      container.append(line);
+    }
+  }
+})
+
+
+//Musical
+$("#Musical").click(function(event) {
+  var buttonPushed = event.target;
+  var genre = Musical //buttonPushed.id;
+  var container = $("#container")
+  container.empty();
+
+  for (var i = 0; i < movies.length; i++) {
+    if (movies[i].Musical) //[genre])
+    {
+
+      var movieImg = $("<img>");
+      if (movies[i].image) {
+        movieImg.attr('src', movies[i].image);
+      } else {
+        movieImg.attr('src', "http://lorempixel.com/32/32");
+      }
+      container.append(movieImg);
+
+      var title = $('<h3>');
+      title.text(movies[i].title);
+      container.append(title);
+
+      var format = $("<div>");
+      format.text(movies[i].format);
+      format.addClass("format");
+      container.append(format);
+
+      var rating = $("<p>");
+      rating.text(movies[i].rating);
+      container.append(rating);
+
+      var notes = $("<p>");
+      notes.text(movies[i].notes);
+      container.append(notes);
+
+      var line = ("<hr>");
+      container.append(line);
+    }
+  }
+})
+
+
+// Kids
+$("#Kids").click(function(event) {
+  var buttonPushed = event.target;
+  var genre = Kids //buttonPushed.id;
+  var container = $("#container")
+  container.empty();
+
+  for (var i = 0; i < movies.length; i++) {
+    if (movies[i].Kids) //[genre])
+    {
+
+      var movieImg = $("<img>");
+      if (movies[i].image) {
+        movieImg.attr('src', movies[i].image);
+      } else {
+        movieImg.attr('src', "http://lorempixel.com/32/32");
+      }
+      container.append(movieImg);
+
+      var title = $('<h3>');
+      title.text(movies[i].title);
+      container.append(title);
+
+      var format = $("<div>");
+      format.text(movies[i].format);
+      format.addClass("format");
+      container.append(format);
+
+      var rating = $("<p>");
+      rating.text(movies[i].rating);
+      container.append(rating);
+
+      var notes = $("<p>");
+      notes.text(movies[i].notes);
+      container.append(notes);
+
+      var line = ("<hr>");
+      container.append(line);
+    }
+  }
+})
+
+
+// Workout
+
+$("#Workout").click(function(event) {
+  var buttonPushed = event.target;
+  var genre = Workout //buttonPushed.id;
+  var container = $("#container")
+  container.empty();
+
+  for (var i = 0; i < movies.length; i++) {
+    if (movies[i].Workout) //[genre])
+    {
+
+      var movieImg = $("<img>");
+      if (movies[i].image) {
+        movieImg.attr('src', movies[i].image);
+      } else {
+        movieImg.attr('src', "http://lorempixel.com/32/32");
+      }
+      container.append(movieImg);
+
+      var title = $('<h3>');
+      title.text(movies[i].title);
+      container.append(title);
+
+      var format = $("<div>");
+      format.text(movies[i].format);
+      format.addClass("format");
+      container.append(format);
+
+      var rating = $("<p>");
+      rating.text(movies[i].rating);
+      container.append(rating);
+
+      var notes = $("<p>");
+      notes.text(movies[i].notes);
+      container.append(notes);
+
+      var line = ("<hr>");
+      container.append(line);
+    }
+  }
+})
